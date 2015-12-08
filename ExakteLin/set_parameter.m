b45dec205b9e82d7549017607c3ea00c3203a228
Andreas <abrewner@users.noreply.github.com>
2015-12-08 12:47:04 +0100

    A4.5 Lineariserung und Ruhelagen

--- ExakteLin/set_parameter.m
+++ ExakteLin/set_parameter.m
@@ -11,9 +11,9 @@
 % close all;
 
 % Parameter des Systems
-parSys.Atank     = 1.539e-2;      % Grundfläche Tank
+parSys.Atank     = 1.539e-2;      % Grundfl?che Tank
 parSys.rho       = 1000;          % Dichte Wasser
-parSys.eta       = 8.9e-4;        % Dynamische Viskosität Wasser
+parSys.eta       = 8.9e-4;        % Dynamische Viskosit?t Wasser
 parSys.g         = 9.81;          % Gravitationskonstante
 parSys.alphaA1   = 0.0583;        % Kontraktionskoeffizient AV1
 parSys.DA1       = 15e-3;         % Durchmesser AV1
@@ -30,6 +30,7 @@ parSys.D23       = 15e-3;         % Durchmesser ZV23
 parSys.lambdac23 = 29600;         % Kritische Fliesszahl ZV23
 parSys.hmin      = 0.05;          % Minimale Fuellhoehe
 parSys.hmax      = 0.55;          % Maximale Fuellhoehe
+parSys.dh12min   = 0.1e-3; 
 
 % Abtastzeit
 parSys.Ta = 0.2;                
@@ -45,4 +46,8 @@ parSys.qZ3max = 4.5e-3/60;        % Maximaler Zufluss Z3
 parSys.qZ1min = 0;                % Minimaler Zufluss Z1
 parSys.qZ3min = 0;                % Minimaler Zufluss Z3
 
-Sollwertfiler;
+Sollwertfilter;
+
+% Ruhelagen
+h10 = .1800312371;
+qZ10 = 0.3454039310e-4;
